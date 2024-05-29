5a1e306f714f6cf65f14a4e4c18cd1bea45f9349
Jesse <frejes32@gmail.com>
2024-05-28 20:12:31 -0500

    Added search bar

---
+++ pages/search/[searchInput].js
@@ -0,0 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../../utils/context/authContext';
+import { searchMedia } from '../../api/mediaData';
+import MediaCard from '../../components/MediaCard';
+
+export default function Search() {
+  const [searchResults, setSearchResults] = useState([]);
+  const { user } = useAuth();
+
+  const router = useRouter();
+  const { searchInput } = router.query;
+
+  const getSearchResults = async () => {
+    const filteredResults = await searchMedia(searchInput, user.uid);
+    setSearchResults(filteredResults);
+  };
+
+  useEffect(() => {
+    getSearchResults();
+  }, [searchInput, user.uid]);
+
+  return (
+    <div className="d-flex flex-wrap">
+      {searchResults.length === 0
+        ? (<h1>No media found.</h1>)
+        : (searchResults.map((results) => (
+          <MediaCard key={results.firebaseKey} mediaObj={results} onUpdate={getSearchResults} />)))}
+    </div>
+  );
+}
