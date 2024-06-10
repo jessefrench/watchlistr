a35892d5e23acdd3fcbc601b3e8616fa8878209e
Jesse <frejes32@gmail.com>
2024-06-10 18:30:06 -0500

    Added new logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import {
   Navbar, Container, Nav, Button,
+  Image,
 } from 'react-bootstrap';
 import { FiEdit } from 'react-icons/fi';
 import SearchBar from './SearchBar';
@@ -13,7 +14,16 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" variant="dark" className="nav">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand className="nav-brand">watchlistr</Navbar.Brand>
+          <Navbar.Brand className="nav-brand">
+            <Image
+              src="/images/logo.png"
+              alt="Logo"
+              width={60}
+              height={50}
+              style={{ marginRight: '10px' }}
+            />
+            watchlistr
+          </Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
