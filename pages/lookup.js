dedfeffa075b209e1d4b7fee853b0371ed1b6374
Jesse French <frejes32@gmail.com>
2024-10-29 11:33:10 -0500

    Improved mobile responsiveness

--- pages/lookup.js
+++ pages/lookup.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Head from 'next/head';
 import LookupForm from '../components/forms/LookupForm';
 
-const Lookup = () => (
-  <>
-    <Head>
-      <title>Watchlistr | Lookup</title>
-    </Head>
-    <div style={{ width: '75%', margin: '0 auto', textAlign: 'center' }}>
-      <h1>TV Show and Movie Lookup</h1>
-      <LookupForm />
-    </div>
-  </>
-);
-
-export default Lookup;
+export default function Lookup() {
+  return (
+    <>
+      <Head>
+        <title>Watchlistr | Lookup</title>
+      </Head>
+      <div className="lookup-page">
+        <h1>TV Show and Movie Lookup</h1>
+        <LookupForm />
+      </div>
+    </>
+  );
+}
